21f3e2287d461c692c345f8466991be17778c632
unknown <juny.joel@gmail.com>
2025-05-08 21:27:11 -0600

    primeras pruebas

--- src/com/alura/desafiostarwars/principal/Main.java
+++ src/com/alura/desafiostarwars/principal/Main.java
@@ -1,7 +1,14 @@
 package com.alura.desafiostarwars.principal;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundirijillo");
+//        System.out.println("Hola Mundirijillo");
+        Scanner keyboardInput = new Scanner(System.in);
+        System.out.println("Escriba numero de episodio");
+        Integer episodio = keyboardInput.nextInt();
+
+        System.out.println("Elegiste el episodio " + episodio);
     }
 }
